a7b75e57475a148557ba37210bb5c20b5a1e439d
Dan Chiche <dan.chiche@signature.one>
2025-09-08 16:11:23 +0200

    Prepare Settle (Need New Bazooka for replace All LCID by new table Culture)

---
+++ CK.DB.Globalization/Res/Model/CK.CultureTable.Settle.1.0.0.sql
@@ -0,0 +1,6 @@
+--[beginscript]
+
+insert into CK.tCulture values( 210333265, 'fr', 'fr', N'French', N'français', N'French', 1, null );
+insert into CK.tCulture values( 221277614, 'en', 'en', N'English', N'English', N'English', 1, null );
+
+--[endscript]
