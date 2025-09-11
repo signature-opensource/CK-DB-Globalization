733e0d82f6de2acd637585e910136a52653e3502
Dan Chiche <dan.chiche@signature.one>
2025-09-11 15:36:44 +0200

    prepare settle (waiting for release ColumnBazookation)

--- CK.DB.Globalization/Res/Model/CK.CultureTable.Settle.1.0.0.sql
+++ CK.DB.Globalization/Res/Model/CK.CultureTable.Settle.1.0.0.sql
@@ -3,4 +3,17 @@
 insert into CK.tCulture values( 210333265, 'fr', 'fr', N'French', N'français', N'French', 1, null );
 insert into CK.tCulture values( 221277614, 'en', 'en', N'English', N'English', N'English', 1, null );
 
+
+exec CKCore.sRefBazookation 'CK','tLCID','LCID','9','221277614';
+exec CKCore.sRefBazookation 'CK','tLCID','LCID','12','210333265';
+
+--exec CKCore.sColumnBazookation
+--    'CK',
+--    'tLCID',
+--    'LCID',
+--    'CK',
+--    'tCulture',
+--    'CultureId',
+--    'FK_CK_{SOURCETABLE}_CultureId foreign key (CultureId) references CK.tCulture(CultureId)'
+
 --[endscript]
