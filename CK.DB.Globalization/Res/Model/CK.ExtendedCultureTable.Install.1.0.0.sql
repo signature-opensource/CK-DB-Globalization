a7af965c72c0bcccc49e20093961b0503d933747
Dan Chiche <dan.chiche@signature.one>
2026-05-29 16:33:04 +0200

    Find a way in Bazooka to avoid to do the drop constraint manually for FK_CK_LCID_XLCID

--- CK.DB.Globalization/Res/Model/CK.ExtendedCultureTable.Install.1.0.0.sql
+++ CK.DB.Globalization/Res/Model/CK.ExtendedCultureTable.Install.1.0.0.sql
@@ -63,25 +63,6 @@ insert into CK.tExtendedCulture( ExtendedCultureId, FullName, PrimaryCultureId )
 insert into CK.tExtendedCulture( ExtendedCultureId, FullName, PrimaryCultureId ) values(  960831636,  'zh-hk,zh-hant,zh', null );
 insert into CK.tExtendedCulture( ExtendedCultureId, FullName, PrimaryCultureId ) values( 1001725972,  'zh-tw,zh-hant,zh', null );
 
--- PK_CK_LCID est référencée par 4 FKs externes au scope du bazookage :
--- FK_CK_MCResHtml_LCID (sur tMCResHtml.LCID)
--- FK_CK_MCResString_LCID (sur tMCResString.LCID)
--- FK_CK_MCResText_LCID (sur tMCResText.LCID)
--- FK_tWorkspaceInvitation_LCID (sur tWorkspaceInvitation.LCID)
--- 
--- SQL Server refuse de drop une PK qui est encore référencée.
---Pour pouvoir drop PK_CK_LCID, il faudrait d'abord drop ces 4 FKs. Mais ces FKs ne sont pas dans rec (elles pointent vers tLCID.LCID, pas vers tXLCID.XLCID), donc le script n'en sait rien.
-
-if exists (
-    select 1
-    from sys.foreign_keys
-    where name             = 'FK_CK_LCID_XLCID'
-      and parent_object_id = OBJECT_ID('CK.tLCID')
-)
-begin
-    alter table CK.tLCID drop constraint FK_CK_LCID_XLCID;
-end
-
 -- BAZOOKA: legacy XLCID -> ExtendedCultureId remapping for the XLCID identifiers.
 -- DE
 exec CKCore.sRefBazookation 'CK','tXLCID','XLCID','7','223893631',0;
